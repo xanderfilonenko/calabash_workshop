4c3c249fa13e0c74eed5d0246acad6444c93cb44
byjok <rkovalchuk@cognince.com>
2015-07-18 16:46:45 +0300

    added elements for workout page

--- features/pages/android/workouts_page.rb
+++ features/pages/android/workouts_page.rb
@@ -12,15 +12,34 @@ end
 
 def dropdowns
 	{
-		"workouts filter" => "* id:''"
+		"workouts filter" => "* id:'filter_selector'"
+		"Treadmill workouts option" => "* text:'Treadmill workouts'"
+		"Non Tread Cardio option" => "* text:'Non Tread Cardio'"
+		"Manually added option" => "* text:'Manually added'"
+		"Tracking App option" => "* text:'Tracking App'"
+		"Traking Device option" => "* text:'Tracking Device'"
 	}
 end
 
-def tap_on_log_in
-	tap_on("log in button")
+def list
+	{
+		"Workout item " => "* text:'#{@temp_var}' parent * id:'main_item'"
+	}
 end
 
+
 def await
-	element_should_be_present("log in button")
+	element_should_be_present("")
 	self
+end
+
+def long_tab_workout_list_item(workout_date)
+	@temp_var = workout_date
+	long_tap_on("Workout item")
+end
+
+def get_workout_info(workout_date,info)
+	
+end
+
 end

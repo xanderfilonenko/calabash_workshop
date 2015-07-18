e542f6911ca9f436390d23da96087b4e8e867123
byjok <rkovalchuk@cognince.com>
2015-07-18 16:04:56 +0300

    workouts page

--- features/pages/android/workouts_page.rb
+++ features/pages/android/workouts_page.rb
@@ -1,1 +1,26 @@
-workouts_page.rb
+require 'calabash-android/abase'
+# This is page class for Start page
+class WorkoutsPage < BasePage
+
+# LOCATORS
+def buttons
+	{
+		"new workout" => "* id:'menu_add_workout'"
+
+	}
+end
+
+def dropdowns
+	{
+		"workouts filter" => "* id:''"
+	}
+end
+
+def tap_on_log_in
+	tap_on("log in button")
+end
+
+def await
+	element_should_be_present("log in button")
+	self
+end

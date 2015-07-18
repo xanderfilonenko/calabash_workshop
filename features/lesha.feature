3164fbd807c565849c8ca221b2ac064d53b6d5ab
Aleksey Tretyak <aleksey.tretyak@gmail.com>
2015-07-18 14:36:34 +0300

    help page

---
+++ features/lesha.feature
@@ -0,0 +1,7 @@
+Feature: Skip tour on Help Page and open Home Page
+
+	Scenario: Skip tour on Help Page
+	Given I Open Start Page
+	When I tap on log in button
+	Then Login page is loaded
+	
---
+++ features/pages/android/help_page.rb
@@ -0,0 +1,33 @@
+# -*- encoding : utf-8 -*-
+
+# This is Parent class for all classes where we have everywhere usefull functions
+class HelpPage < BasePage
+
+  def fields 
+    {
+      
+    }
+  end
+
+  def dropdowns
+    {
+      
+    }
+  end
+
+  def links
+    {
+      
+    }
+  end
+
+  def buttons
+    {
+      "Go to App button" => "Button id:'inapptour_skip'"
+    }
+  end
+
+  def await
+    element_should_be_present("Go to App button")
+    self
+  end

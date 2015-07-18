4554b4cd626eff5e92c4b52425e6321b191ef957
Volodymyr Kavun <vkavun@cogniance.com>
2015-07-18 13:53:01 +0300

    Added date pick step

--- features/step_definitions/calabash_steps.rb
+++ features/step_definitions/calabash_steps.rb
@@ -1,1 +1,5 @@
 require 'calabash-android/calabash_steps'
+
+Then(/^I pick day (.*) and month (.*) and year (.*)$/) do |day, month, year|
+  @current_page.pick_date(date_picker, day, month, year)
+end

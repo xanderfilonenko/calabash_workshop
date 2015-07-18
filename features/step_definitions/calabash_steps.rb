004e6c293bb1212595c7e8901a75a89e92563ed6
Aleksey Tretyak <aleksey.tretyak@gmail.com>
2015-07-18 13:59:09 +0300

    Element should or should not be on the screen

--- features/step_definitions/calabash_steps.rb
+++ features/step_definitions/calabash_steps.rb
@@ -1,1 +1,13 @@
 require 'calabash-android/calabash_steps'
+
+When(/^ I open Login page$/) do
+	
+end
+
+Then(/^(.*) should be on the screen/) do |element|
+	element_should_be_present(element)
+end
+
+Then(/^(.*) should not be on the screen/) do |element|
+	element_should_not_be_present(element)
+end
